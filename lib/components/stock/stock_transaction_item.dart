33388b044b6e3f5644016924095e1b68af35823f
oJaciel <jacielhbarichellO@gmail.com>
2025-11-03 20:36:57 -0300

    Melhoria TransactionItem
    Melhorada a visualização das transações de estoque.

--- lib/components/stock/stock_transaction_item.dart
+++ lib/components/stock/stock_transaction_item.dart
@@ -13,8 +13,6 @@ class StockTransactionItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final formattedHour = DateFormat('HH:mm').format(transaction.date);
-
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 0),
       child: ListTile(
@@ -28,62 +26,79 @@ class StockTransactionItem extends StatelessWidget {
         ),
         title: Row(
           children: [
-            Text(
-              transaction.product.name,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Spacer(),
-            Icon(
-              Icons.watch_later_rounded,
-              size: 16,
-              color: Theme.of(context).primaryColor,
-            ),
+            Text(DateFormat('dd/MM').format(transaction.date)),
+            SizedBox(width: 4),
+            Icon(Icons.circle, size: 4),
             SizedBox(width: 4),
             Text(
-              formattedHour,
-              style: TextStyle(
-                fontSize: 14,
-                color: Theme.of(context).primaryColor,
-              ),
+              DateFormat('HH:mm').format(transaction.date),
+              style: TextStyle(fontSize: 16),
             ),
           ],
         ),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Quantidade: ${transaction.quantity}'),
-            Text(
-              'Tipo: ${transaction.type == TransactionType.entry ? 'Entrada' : 'Saída'}',
+            Row(
+              children: [
+                Text('Produto:', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(width: 4),
+                Text(transaction.product.name),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  'Quantidade:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 4),
+                Text(transaction.quantity.toString()),
+              ],
+            ),
+            Row(
+              children: [
+                Text('Tipo:', style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(width: 4),
+                Text(
+                  transaction.type == TransactionType.entry
+                      ? 'Entrada'
+                      : 'Saída',
+                ),
+              ],
             ),
           ],
         ),
+        trailing: SizedBox(
+          height: 30,
+          width: 30,
+          child: IconButton(
+            onPressed: () => showDialog(
+              context: context,
+              builder: (ctx) => DeleteAlertDialog(
+                title: 'Excluir',
+                content: 'Deseja realmente excluir?',
+                deleteMethod: () async {
+                  final stockProvider = Provider.of<StockProvider>(
+                    context,
+                    listen: false,
+                  );
+                  final productProvider = Provider.of<ProductProvider>(
+                    context,
+                    listen: false,
+                  );
 
-        trailing: IconButton(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (ctx) => DeleteAlertDialog(
-              title: 'Excluir',
-              content: 'Deseja realmente excluir?',
-              deleteMethod: () async {
-                final stockProvider = Provider.of<StockProvider>(
-                  context,
-                  listen: false,
-                );
-                final productProvider = Provider.of<ProductProvider>(
-                  context,
-                  listen: false,
-                );
-
-                await stockProvider.removeTransaction(
-                  context,
-                  productProvider,
-                  transaction,
-                );
-              },
+                  await stockProvider.removeTransaction(
+                    context,
+                    productProvider,
+                    transaction,
+                  );
+                },
+              ),
             ),
+            icon: Icon(Icons.delete_rounded, color: Colors.red),
+            iconSize: 30,
           ),
-          icon: Icon(Icons.delete_rounded, color: Colors.red),
-          iconSize: 30,
         ),
       ),
     );
